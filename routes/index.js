a4c26e07d74d766ec13355c755f7b0cbfbc2b903
The_Costa <bryan.costa@roadrunner.com>
2019-05-18 20:56:29 -0700

    Corrects Layouts folder

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ const db = require("../config");
 
 module.exports = app => {
   // your routes here...
-  app.get("/", (req, res) => res.render("index"));
+  app.get("/burger", (req, res) => res.render("index"));
 };
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const db = require("mysql2");
 const { join } = require("path");
 const app = express();
 
 app.use(express.static(join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine(".hbs", require("express-handlebars")({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  require("express-handlebars")({ defaultLayout: "main", extname: ".hbs" })
+);
 app.set("view engine", ".hbs");
 
 require("./routes")(app);
 
 require("./config").connect(_ => {
   app.listen(3000);
-});
+}, console.log("its working"));
